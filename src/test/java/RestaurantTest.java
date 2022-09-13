fbe34b061791e590b8b6e4bfa06989357fd0a4d3
meatgh <rahmanmuti@gmail.com>
2022-09-13 17:23:50 +0530

    part 2: Solution

---
+++ src/test/java/RestaurantTest.java
@@ -0,0 +1,98 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class RestaurantTest {
+    Restaurant restaurant;
+    //REFACTOR ALL THE REPEATED LINES OF CODE
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
+    @Test
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+        //WRITE UNIT TEST CASE HERE
+        Restaurant restaurant1 = Mockito.mock(Restaurant.class);
+        LocalTime openTime = LocalTime.now().minusHours(3);
+        LocalTime closeTime = LocalTime.now().plusHours(9);
+        String name = "Bawarchi";
+        String location = "Hyderabad";
+
+        Mockito.when(restaurant1.openingTime).thenReturn(openTime);
+        Mockito.when(restaurant1.closingTime).thenReturn(closeTime);
+        Mockito.when(restaurant1.getName()).thenReturn(name);
+        Mockito.when(restaurant1.getLocation()).thenReturn(location);
+
+        assertTrue(restaurant1.isRestaurantOpen());
+
+
+
+    }
+
+    @Test
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+        //WRITE UNIT TEST CASE HERE
+
+        Restaurant restaurant1 = Mockito.mock(Restaurant.class);
+        LocalTime openTime = LocalTime.now().plusHours(4);
+        LocalTime closeTime = LocalTime.now().plusHours(-2);
+        String name = "Bawarchi";
+        String location = "Hyderabad";
+
+        Mockito.when(restaurant1.openingTime).thenReturn(openTime);
+        Mockito.when(restaurant1.closingTime).thenReturn(closeTime);
+        Mockito.when(restaurant1.getName()).thenReturn(name);
+        Mockito.when(restaurant1.getLocation()).thenReturn(location);
+
+        assertFalse(restaurant1.isRestaurantOpen());
+
+    }
+
+    //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Vegetable lasagne");
+        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_that_does_not_exist_should_throw_exception() {
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        assertThrows(itemNotFoundException.class,
+                ()->restaurant.removeFromMenu("French fries"));
+    }
+    //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+}
